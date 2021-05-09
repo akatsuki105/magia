91a6d25c6b8c4eebd98feb69775f4bba8df6ff55
pokemium <bluejapan73+dev@gmail.com>
2021-05-10 03:17:11 +0900

    fix

--- pkg/gba/cond.go
+++ pkg/gba/cond.go
@@ -23,6 +23,10 @@ const (
 	NV
 )
 
+var condLut = [16]uint16{
+	0xF0F0, 0x0F0F, 0xCCCC, 0x3333, 0xFF00, 0x00FF, 0xAAAA, 0x5555, 0x0C0C, 0xF3F3, 0xAA55, 0x55AA, 0x0A05, 0xF5FA, 0xFFFF, 0x0000,
+}
+
 var cond2str = map[Cond]string{EQ: "eq", NE: "ne", CS: "cs", CC: "cc", MI: "mi", PL: "pl", VS: "vs", VC: "vc", HI: "hi", LS: "ls", GE: "ge", LT: "lt", GT: "gt", LE: "le", AL: "al", NV: "nv"}
 
 func (c Cond) String() string {
@@ -34,40 +38,6 @@ func (c Cond) String() string {
 
 // Check returns if instruction condition is ok
 func (g *GBA) Check(cond Cond) bool {
-	switch cond {
-	case EQ:
-		return g.GetCPSRFlag(flagZ)
-	case NE:
-		return !g.GetCPSRFlag(flagZ)
-	case CS:
-		return g.GetCPSRFlag(flagC)
-	case CC:
-		return !g.GetCPSRFlag(flagC)
-	case MI:
-		return g.GetCPSRFlag(flagN)
-	case PL:
-		return !g.GetCPSRFlag(flagN)
-	case VS:
-		return g.GetCPSRFlag(flagV)
-	case VC:
-		return !g.GetCPSRFlag(flagV)
-	case HI:
-		return g.GetCPSRFlag(flagC) && !g.GetCPSRFlag(flagZ)
-	case LS:
-		return (!g.GetCPSRFlag(flagC)) || g.GetCPSRFlag(flagZ)
-	case GE:
-		return g.GetCPSRFlag(flagN) == g.GetCPSRFlag(flagV)
-	case LT:
-		return g.GetCPSRFlag(flagN) != g.GetCPSRFlag(flagV)
-	case GT:
-		return !g.GetCPSRFlag(flagZ) && (g.GetCPSRFlag(flagN) == g.GetCPSRFlag(flagV))
-	case LE:
-		return g.GetCPSRFlag(flagZ) || (g.GetCPSRFlag(flagN) != g.GetCPSRFlag(flagV))
-	case AL:
-		return true
-	case NV:
-		return false
-	default:
-		return false
-	}
+	flags := g.Reg.CPSR >> 28
+	return (condLut[cond] & (1 << flags)) != 0
 }
